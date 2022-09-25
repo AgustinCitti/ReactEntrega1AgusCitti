d1b98b818dec8dd644183f953e68cb754217be36
AgustinCitti <aguscitti@gmail.com>
2022-09-25 19:13:55 -0300

    react

--- src/components/Pages/Nosotros.js
+++ src/components/Pages/Nosotros.js
@@ -1,4 +1,20 @@
-const Nosotros = () => {
+import {db} from "../../utils/firebase";
+import {doc, getDoc} from "firebase/firestore";
+import React, {useContext, useEffect} from 'react';
+ const Nosotros = () => {
+useEffect(()=>{
+ const getData = async()=>{
+//creamos la referencia
+    const query = doc(db, "Items", "4ihnjsDNd2UTalSH23xN");
+    //solicitud
+    const response = await getDoc(query);
+    console.log("response", response);
+ }
+ getData();   
+},[])
+
+
+
     return (
         <div>
             <h1>Nuestra Historia</h1>
