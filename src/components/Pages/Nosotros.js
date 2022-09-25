c157105a9f94df6a955efc7b289dc3482ea72756
AgustinCitti <aguscitti@gmail.com>
2022-09-25 20:04:04 -0300

    react

--- src/components/Pages/Nosotros.js
+++ src/components/Pages/Nosotros.js
@@ -1,20 +1,68 @@
 import {db} from "../../utils/firebase";
-import {doc, getDoc} from "firebase/firestore";
+import {doc, getDoc, collection, getDocs, query, where, limit} from "firebase/firestore";
 import React, {useContext, useEffect} from 'react';
+
+
+
  const Nosotros = () => {
+    
+// useEffect(()=>{
+//  const getData = async()=>{
+// //creamos la referencia
+//     const query = doc(db, "Items", "4ihnjsDNd2UTalSH23xN");
+//     //solicitud
+//     const response = await getDoc(query);
+//     console.log("response", response.id);
+//     console.log("data", response.data());
+//     const newProduct = {
+//         ...response.data(),
+//         id: response.id
+//     }
+//     console.log(newProduct);
+//  }
+//  getData();   
+// },[])
+
+// useEffect(()=>{
+//     const getData = async()=>{
+//    //creamos la referencia
+//       const query = collection(db, "Items");
+//        //solicitud
+//         const response = await getDocs(query);
+//         console.log(response);
+//         const productos = response.docs.map(doc =>{
+//             const newProduct = {
+//              ...doc.data(),
+//               id: doc.id
+//               }
+//               return newProduct
+//         })
+//         console.log(productos);
+//     }
+//     getData();   
+//    },[])
+
+
+
+
 useEffect(()=>{
- const getData = async()=>{
-//creamos la referencia
-    const query = doc(db, "Items", "4ihnjsDNd2UTalSH23xN");
-    //solicitud
-    const response = await getDoc(query);
-    console.log("response", response);
- }
- getData();   
-},[])
-
-
-
+    const getData = async()=>{
+   //creamos la referencia
+      const queryRef = query(collection(db,"Items"),where("category", "==","cervezaLiviana"),limit(1));
+       //solicitud
+        const response = await getDocs(queryRef);
+        console.log(response);
+        const productos = response.docs.map(doc =>{
+            const newProduct = {
+             ...doc.data(),
+              id: doc.id
+              }
+              return newProduct
+        })
+        console.log(productos);
+    }
+    getData();   
+   },[])
     return (
         <div>
             <h1>Nuestra Historia</h1>
